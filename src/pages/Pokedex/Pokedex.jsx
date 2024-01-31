24ffef9934e3ca82247337aa7ee36eaf4efa710a
Jia Wang <daisy-a.119@hotmail.com>
2024-01-31 00:19:13 -0500

    implement filter function

--- src/pages/Pokedex/Pokedex.jsx
+++ src/pages/Pokedex/Pokedex.jsx
@@ -8,7 +8,16 @@ console.log(pokeData)
 const Pokedex = () => {
   const displayCount = 10
   const [currIdx, setCurrIdx] = useState(0)
-  const [displayedPokemon, setDisplayedPokemon] = useState(pokeData)
+  const [displayedPokemon, setDisplayedPokemon] = useState(filterPokemonData(0))
+
+  function filterPokemonData(newIdx) {
+    //filter pokeData to a new array of 10 pokemon (newIdx+displayCount)
+    const filteredData = pokeData.filter((pokemon, idx) => {
+      //falsey: 10th = > 10 >= 0 10 !< 0+10
+      return idx >=newIdx && idx < newIdx + displayCount
+    })
+    return filteredData
+  }
 
   return ( 
     <>
