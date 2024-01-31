1bed51fe40cd5d79f3c33a23c8ca6bae83f5b736
Jia Wang <daisy-a.119@hotmail.com>
2024-01-30 23:13:05 -0500

    finish part 1

--- src/pages/ContactInfo/ContactInfo.jsx
+++ src/pages/ContactInfo/ContactInfo.jsx
@@ -3,7 +3,18 @@ import './ContactInfo.css'
 const ContactInfo = (props) => {
   return ( 
     <>
-    <h4>{props.contact.email}</h4>
+    <h1>{props.contact.company.name}</h1>
+    <h4 id="yellow">"{props.contact.company.catchPhrase}"</h4>
+    <div className='email-phone'>
+      <p>{props.contact.email}</p>
+      <p>{props.contact.phone}</p>
+      <p>{props.contact.website}</p>
+    </div>
+    <div className='address'>
+      <p>{props.contact.address.street}</p>
+      <p>{props.contact.address.suite}</p>
+      <p>{props.contact.address.city}{'\u00A0'}{props.contact.address.zipcode}</p>
+    </div>
     </>
   )
 }
--- src/pages/UserCard/UserCard.jsx
+++ src/pages/UserCard/UserCard.jsx
@@ -1,14 +1,21 @@
 import './UserCard.css'
 import ProfileInfo from '../ProfileInfo/ProfileInfo';
 import ContactInfo from '../ContactInfo/ContactInfo';
+import { useState } from 'react';
 
 const UserCard = (props) => {
+  const [displayProfileInfo, setShowContact] = useState(true)
+
+  function handleContactDisplay() {
+    setShowContact(!displayProfileInfo)
+  }
   return ( 
     <div className="user-card">
-      <ProfileInfo profile={props.user} />
-      <ContactInfo contact={props.user} />
-      
-    
+      {displayProfileInfo ? <ProfileInfo profile={props.user} /> : <ContactInfo contact={props.user} />}
+
+      {displayProfileInfo ? '':<button id='website'>Visit Website</button> }
+
+      <button id="contact-btn" onClick={handleContactDisplay}>{displayProfileInfo? "Show " : "Hide"}Contact Info</button>
     </div>
    );
 }
