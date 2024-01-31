f45956f1cdb281420ceb7aee5f4340a2bcd84eb2
Jia Wang <daisy-a.119@hotmail.com>
2024-01-30 21:27:35 -0500

    add css for card set up

--- src/pages/UserCard/UserCard.jsx
+++ src/pages/UserCard/UserCard.jsx
@@ -1,8 +1,11 @@
+import './UserCard.css'
+
 const UserCard = (props) => {
   return ( 
-    <>
+    <div className="user-card">
+    <img src={props.user.avatar} alt="avatar" />
     <h2>{props.user.name}</h2>
-    </>
+    </div>
    );
 }
  
