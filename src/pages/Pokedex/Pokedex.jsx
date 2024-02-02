924fd5e63cc2e25f57d9dff4ad782d8641448c16
Jia Wang <daisy-a.119@hotmail.com>
2024-02-02 01:21:28 -0500

    add more comment for part 2

--- src/pages/Pokedex/Pokedex.jsx
+++ src/pages/Pokedex/Pokedex.jsx
@@ -14,29 +14,39 @@ const Pokedex = () => {
   function filterPokemonData(newIndex) {
     //filter pokeData to a new array of 10 pokemon (newIndex+displayCount)
     const filteredData = pokeData.filter((pokemon, idx) => {
-      //falsey: 10th = > 10 >= 0 10 !< 0+10
       return idx >=newIndex && idx < newIndex + displayCount
     })
     return filteredData
   }
   
   function handleIncreaseCurrIdx() {
+    //newIdx is the new starting index of pokeData array
     const newIdx =currIdx + displayCount
+    //make sure newIdx is within the range of pokeData
     if (newIdx >= pokeData.length) {
       return
     }
+    //this var is the result of running filterPokemonData(newIdx)
     const filteredPokemonData = filterPokemonData(newIdx)
+    //updaye cuurIdx using setCurreIdx with newIdx
     setCurrIdx(newIdx)
+    //update displayedPokemon using setDisplayedPokemon with filteredPokemonData
     setDisplayedPokemon(filteredPokemonData)
   }
 
   function handleDecreaseCurrIdx() {
+    //newIdx is the new starting indox of pokeData 
+    //(subtraction bc it is backward)
   const newIdx =currIdx - displayCount
+  //if the newIdx is out of the range then return out of the function
   if (newIdx < 0 ) {
     return
   }
+  //this var is the result of running filterPokemonData(newIdx)
   const filteredPokemonData = filterPokemonData(newIdx)
+  //updaye cuurIdx using setCurreIdx with newIdx
   setCurrIdx(newIdx)
+  //update displayedPokemon using setDisplayedPokemon with filteredPokemonData
   setDisplayedPokemon(filteredPokemonData)
   }
 
@@ -51,12 +61,14 @@ const Pokedex = () => {
   return ( 
     <>
       <h1>Pokemon List</h1>
+
       <div className="search-container">
         <p>Search:</p>
         <input onChange={onChange} type="search" />
         <h3>Results:</h3>
         {searchItems.map(item => (<div>{item}</div>))}
       </div>
+
       <div className="pagination-container">
         <button 
         id="backward"
@@ -72,6 +84,7 @@ const Pokedex = () => {
         </button>
       </div>
       <div className="num-results-container">
+        {/* display the results range */}
         Results {currIdx +1} - {currIdx + displayCount} of {pokeData.length}
       </div>
       <div className="pokemon-container">
